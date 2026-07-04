06f767b29c09e468ac4ff9135a2042f4dc81af73
Sultan2403 <sultanremilekun@gmail.com>
2026-07-04 22:28:18 +0100

    api layer complete

--- Backend/src/Api/api.client.ts
+++ Backend/src/Api/api.client.ts
@@ -1,11 +1,20 @@
-import axios from "axios"
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
-const url = ""
+const url = "";
 
 const api = axios.create({
-    baseUrl: url
-})
+  baseURL: url,
+});
 
-api.request.interceptors.use((config)=>{
-  
-})
+api.interceptors.request.use((config) => {
+  if (!config.headers.has("User-Agent")) {
+    config.headers.set("User-Agent", "Chess-Vault-Backend/1.0");
+  }
+
+  return config;
+});
+
+// Response interceptor: auto-parse JSON strings and unwrap common `data` envelope
+api.interceptors.response.use((res) => res.data);
+
+export default api;
