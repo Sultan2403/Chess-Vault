4ee27183e85180a8818434d0b3026bf54e3c1bcd
Sultan2403 <sultanremilekun@gmail.com>
2026-07-05 02:17:47 +0100

    removed redundant header check in api client

--- Backend/src/Api/api.client.ts
+++ Backend/src/Api/api.client.ts
@@ -8,12 +8,10 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  if (!config.headers.has("User-Agent")) {
-    config.headers.set(
-      "User-Agent",
-      `Chess-Vault-Backend/1.0 (contact: ${env.DEV_EMAIL})`,
-    );
-  }
+  config.headers.set(
+    "User-Agent",
+    `Chess-Vault-Backend/1.0 (contact: ${env.DEV_EMAIL})`,
+  );
 
   return config;
 });
