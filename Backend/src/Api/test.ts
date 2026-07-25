b0adeb6c8d4d84a016b8d8be42f225bc82b3c452
Sultan2403 <sultanremilekun@gmail.com>
2026-07-25 17:52:08 +0100

    commented out test code

--- Backend/src/Api/test.ts
+++ Backend/src/Api/test.ts
@@ -1,30 +1,30 @@
-import fs from "fs";
-import lichessApi from "./lichess.api";
-import chessComApi from "./chess_com.api";
+// import fs from "fs";
+// import lichessApi from "./lichess.api";
+// import chessComApi from "./chess_com.api";
 
-const getStuff = async () => {
-  try {
-    const chessComRes = await chessComApi.getPlayerGamesForMonth(
-      "Sultan2403",
-      2026,
-      7,
-    );
-    console.log("chess.com response:", chessComRes);
+// const getStuff = async () => {
+//   try {
+//     const chessComRes = await chessComApi.getPlayerGamesForMonth(
+//       "Sultan2403",
+//       2026,
+//       7,
+//     );
+//     console.log("chess.com response:", chessComRes);
 
-    const res = await lichessApi.getUserGames("Sultan2403");
-    console.log("lichess response:", res);
+//     const res = await lichessApi.getUserGames("Sultan2403");
+//     console.log("lichess response:", res);
 
-    fs.writeFileSync(
-      "src/Api/chess.comtest.json",
-      JSON.stringify(chessComRes, null, 2),
-    );
-  } catch (err: any) {
-    if (err.response) {
-      console.error("API error status:", err.response.status);
-      console.error("API error data:", err.response.data);
-    }
-    console.error(err.message);
-  }
-};
+//     fs.writeFileSync(
+//       "src/Api/chess.comtest.json",
+//       JSON.stringify(chessComRes, null, 2),
+//     );
+//   } catch (err: any) {
+//     if (err.response) {
+//       console.error("API error status:", err.response.status);
+//       console.error("API error data:", err.response.data);
+//     }
+//     console.error(err.message);
+//   }
+// };
 
-getStuff();
+// getStuff();
