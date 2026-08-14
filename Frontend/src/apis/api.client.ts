42292913cf27e51c84407f10342439d74c0be172
Sultan2403 <sultanremilekun@gmail.com>
2026-08-14 19:53:55 +0100

    updated api timeout

--- Frontend/src/apis/api.client.ts
+++ Frontend/src/apis/api.client.ts
@@ -5,7 +5,7 @@ const url = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: url,
-  timeout: url?.includes("localhost") ? 5000 : 10000,
+  timeout: 30000,
 });
 
 api.interceptors.request.use(async (config) => {
